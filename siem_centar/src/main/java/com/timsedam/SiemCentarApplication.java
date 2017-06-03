5dd59748e83a4015237f745d9f05dde28e6016f2
Nemanja Zunic <n.zunic@yahoo.com>
2017-06-03 18:42:26 +0200

    siem centar initialized

---
+++ siem_centar/src/main/java/com/timsedam/SiemCentarApplication.java
@@ -0,0 +1,12 @@
+package com.timsedam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiemCentarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiemCentarApplication.class, args);
+	}
+}
